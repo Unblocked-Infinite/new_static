e9562cdaca5fe78995e9a62b946b754363d9737b
stellerNet <stellernet.org@gmail.com>
2023-08-04 10:00:52 -0500

    Update ultraviolet-static submodule

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 "use strict";
+
 /**
  * @type {HTMLFormElement}
  */
@@ -26,6 +27,7 @@ form.addEventListener("submit", async (event) => {
   try {
     await registerSW();
   } catch (err) {
+    document.getElementById("popup").style.display = "block";
     error.textContent = "Failed to register service worker.";
     errorCode.textContent = err.toString();
     throw err;
@@ -34,3 +36,74 @@ form.addEventListener("submit", async (event) => {
   const url = search(address.value, searchEngine.value);
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
+
+var db;
+var request = window.indexedDB.open('myDatabase', 1);
+
+request.onerror = function(event) {
+  console.error('Error opening database:', event.target.errorCode);
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+
+  // Retrieve the stored search engine URL from IndexedDB
+  var transaction = db.transaction(['settings'], 'readonly');
+  var objectStore = transaction.objectStore('settings');
+  var getRequest = objectStore.get('searchEngine');
+
+  getRequest.onsuccess = function(event) {
+    var searchEngineData = event.target.result;
+    var defaultSearchEngineURL = 'https://www.google.com/search?q=%s';
+  
+    if (searchEngineData && searchEngineData.url) {
+      // Set the stored search engine URL as the value for the hidden input
+      document.getElementById('uv-search-engine').value = searchEngineData.url;
+  
+      // Extract the search engine name from the stored URL
+      var searchEngineName = extractSearchEngineName(searchEngineData.url);
+  
+      // Change the background image URL for the .search-logo element
+      var searchLogoElement = document.querySelector('.search-logo');
+      var imageUrl = './assets/' + searchEngineName.toLowerCase() + '.webp';
+      searchLogoElement.style.backgroundImage = 'url("' + imageUrl + '")';
+    } else {
+      // No search engine URL found, use the default search engine URL
+      document.getElementById('uv-search-engine').value = defaultSearchEngineURL;
+  
+      // Extract the search engine name from the default URL
+      var searchEngineName = extractSearchEngineName(defaultSearchEngineURL);
+  
+      // Change the background image URL for the .search-logo element using the default search engine name
+      var searchLogoElement = document.querySelector('.search-logo');
+      var imageUrl = './assets/' + searchEngineName.toLowerCase() + '.webp';
+      searchLogoElement.style.backgroundImage = 'url("' + imageUrl + '")';
+    }
+  };
+  
+};
+
+function extractSearchEngineName(url) {
+  var domain = getDomainFromUrl(url);
+  if (domain === 'duckduckgo.com') {
+    return 'duckduckgo';
+  } else {
+    var searchEngineName = domain.split('.')[1];
+    return searchEngineName;
+  }
+}
+
+
+function getDomainFromUrl(url) {
+  var parser = document.createElement('a');
+  parser.href = url;
+  return parser.hostname;
+}
+
+
+request.onupgradeneeded = function(event) {
+  db = event.target.result;
+
+  // Create an object store to store the background image URL
+  var objectStore = db.createObjectStore('settings', { keyPath: 'id' });
+};
